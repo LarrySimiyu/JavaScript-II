4dae9a2d3a6491aee8076911caa3ba577af78b36
Larry Simiyu <larrysimiyu@larrys-macbook.lan>
2019-01-22 17:01:45 -0800

    changes larry simiyu

--- assignments/closure.js
+++ assignments/closure.js
@@ -11,13 +11,16 @@ function anyFunc(name) {
     }
 }
 
-
 anyFunc("larry");
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    return count += 1;
+  }
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -30,4 +33,15 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  let count = 0;
+
+    return {
+      increment() {
+        return count += 1;
+      }, 
+      decrement() {
+        return count -= 1;
+      }
+    }
 };
